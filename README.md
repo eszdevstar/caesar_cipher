c17cd00fb8098b41c9a7d2234d8c951408838544
eszdevstar <devstaresz@gmail.com>
2022-12-06 02:14:10 -0300

    Make the main definition && update README

--- README.md
+++ README.md
@@ -16,5 +16,20 @@ Arrays, Hashes, Debug.
 
 Enumerable and Collections
 
+## Methods used from the docs
+
+`zip(*other_arrays)` → new_array
+
+: When no block given, returns a new Array new_array of size self.size whose elements are Arrays.
+
+`rotate(count)` → new_array
+
+: Returns a new Array formed from self with elements rotated from one end to the other.
+
+## Methods used from from the course
+
+each_char, map, fetch, join
+
+
 
 ### code with love <3
--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
-puts "Hello, World!"
+
+def caesar_cipher(string, shift = 0)
+  # uses zip to concat two array into a hash and
+  # rotate to iterate through the array for the given number of times
+  encrypt = ([*('a'..'z')].zip([*('a'..'z')].rotate(shift)) + [*('A'..'Z')].zip([*('A'..'Z')].rotate(shift))).to_h
+
+  # uses map fetch and join to make  a new array with the letters changed
+  p string.each_char.map { |char| encrypt.fetch(char, char) }.join
+
+end
+
+caesar_cipher("What a string!", 5)
